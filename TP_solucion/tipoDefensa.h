372ff07c39ce949939a2603df3e2ef0e76a220ae
MartinaHanselmann <martuhanselmann@gmail.com>
2024-06-19 12:42:58 -0300

    enum agregado

--- TP_solucion/tipoDefensa.h
+++ TP_solucion/tipoDefensa.h
@@ -1,1 +1,6 @@
 #pragma once
+#ifndef _TIPODEFENSA_H
+#define _TIPODEFENSA_H
+
+typedef enum{abrazo_de_oso=3, capa_invisibilidad=10, reflejos_rapidos=20, armadura_dragon=5, resistencia_intensa=15}tipoDefensa;
+#endif //_TIPODEFENSA_H
