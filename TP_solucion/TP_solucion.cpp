b70fcff9c751b9a4e07bbaf85eeb54c9fd52b136
bautistarach <rachbauti03@gmail.com>
2024-06-24 18:54:49 -0300

    Finalizado

--- TP_solucion/TP_solucion.cpp
+++ TP_solucion/TP_solucion.cpp
@@ -156,6 +156,36 @@ list<cDragones*>::iterator it3;
    }
     DragonesMigue.clear();
 
+    list<cDragones*>::iterator it4;
+    for (it4 = DragonesTotales.begin(); it4 != DragonesTotales.end(); it4++) {
+        delete(*it4);
+    }
+    DragonesTotales.clear();
+
+    list<cDragones*>::iterator it5;
+
+    for (it5 = DragonesDomados.begin(); it5 != DragonesDomados.end(); it5++) {
+        delete(*it5);
+    }
+    DragonesDomados.clear();
+
+    list<cDragones*>::iterator it6;
+
+    for (it6 = DragonesEliminados.begin(); it6 != DragonesEliminados.end(); it6++) {
+        delete(*it6);
+
+    }
+    DragonesEliminados.clear();
+
+    list<cDragones*>::iterator it7;
+
+    for (it7 = DragonesSalvajes.begin(); it7 != DragonesSalvajes.end(); it7++) {
+        delete(*it7);
+
+    }
+    DragonesSalvajes.clear();
+
+
     delete forma1;
     delete forma2;
     delete forma3;
